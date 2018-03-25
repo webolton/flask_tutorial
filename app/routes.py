5100a774727576470aa0d0ecfb9b9d91a81f456c
William Bolton <william.ellet@gmail.com>
2018-03-25 16:30:39 -0400

    Handle post with flash messages

--- app/routes.py
+++ app/routes.py
@@ -18,7 +18,7 @@ def index():
         ]
     return render_template('index.html', title='Home', user=user, posts=posts)
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
