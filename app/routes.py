38b03eefdd36cb799f6608071b04cda32d560b1a
William Bolton <william.ellet@gmail.com>
2018-03-30 15:28:53 -0400

    Add show user route

--- app/routes.py
+++ app/routes.py
@@ -47,3 +47,13 @@ def register():
         flash('Congratulations, you are now a registered user!')
         return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
+
+@app.route('/user/<username>')
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = [
+        {'author': user, 'body': 'Test test1'},
+        {'author': user, 'body': 'Test test2'}
+    ]
+    return render_template('user.html', user=user, posts=posts)
