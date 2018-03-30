df1c1c71246e92b0908ac4a13484c8adb509c287
William Bolton <william.ellet@gmail.com>
2018-03-30 13:24:04 -0400

    Remove mock data

--- app/routes.py
+++ app/routes.py
@@ -5,17 +5,6 @@ from app.forms import LoginForm
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'username': 'test_flask_user'}
-    posts = [
-            {
-                'author': {'username': 'John'},
-                'body': 'Beautiful day in Portland!'
-            },
-            {
-                'author': {'username': 'Susan'},
-                'body': 'The Avengers movie was so cool!'
-            }
-        ]
     return render_template('index.html', title='Home', user=user, posts=posts)
 
 @app.route('/login', methods=['GET', 'POST'])
