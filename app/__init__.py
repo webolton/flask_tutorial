0802402334d78c8bfeac0041d4036b09450db0fd
William Bolton <william.ellet@gmail.com>
2018-04-01 17:51:53 -0400

    Add logging to file

--- app/__init__.py
+++ app/__init__.py
@@ -4,8 +4,9 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 import logging
-from logging.handlers import SMPTHandler
+from logging.handlers import SMPTHandler, RotatingFileHandler
 from app import routes, models, errors
+import os
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -15,8 +16,8 @@ login = LoginManager(app)
 login.login_view = 'login'
 
 if not app.debug:
-    if app.config['MAIL_SERVER']
-    auth = None
+    if app.config['MAIL_SERVER']:
+        auth = None
     if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
         auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
     secure = None
@@ -29,3 +30,15 @@ if not app.debug:
         credentials=auth, secure=secure)
     mail_handler.setLevel(logging.ERROR)
     app.logger.addHandler(mail_handler)
+
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    file_handler = RotatingFileHandler('logs/flask_tutorial.log', maxBytes=10240,
+                                        backupCount=10)
+    file_handler.setFormatter(logging.Formatter(
+        '%(asctime)s %(levelname)s: %(message)s, [in %(pathname)s:%(lineno)d]'))
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+
+    app.logger.setLevel(loggingINFO)
+    app.logger.info('Flask Tutrial startup')
