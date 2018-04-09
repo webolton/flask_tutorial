0f08cd9cbfa79ace18e0d0ddf060233336ce4cbc
William Bolton <william.ellet@gmail.com>
2018-04-08 21:33:45 -0400

    Implement method to return join table of posts

--- app/models.py
+++ app/models.py
@@ -22,6 +22,12 @@ class User(UserMixin, db.Model):
     about_me = db.Column(db.String(140))
     last_seen = db.Column(db.DateTime, default=datetime.utcnow)
 
+    followed = db.relationship(
+        'User', secondary=followers,
+        primaryjoin=(followers.c.follower_id == id),
+        secondaryjoin=(followers.c.followed_id == id),
+        backref=db.backref('followers', lazy='dynamic'), lazy='dynamic')
+
     def __repr__(self):
         return '<User {}, {}>'.format(self.username, self.email)
 
@@ -34,12 +40,6 @@ class User(UserMixin, db.Model):
     def avatar(self):
         return url_for('static', filename='img/python.png')
 
-    followed = db.relationship(
-        'User', secondary=followers,
-        primaryjoin=(followers.c.follower_id == id),
-        secondaryjoin=(followers.c.follower_id == id),
-        backref=db.backref('followers', lazy='dynamic'), lazy='dynamic')
-
     def follow(self, user):
         if not self.is_following(user):
             self.followed.append(user)
@@ -51,6 +51,14 @@ class User(UserMixin, db.Model):
     def is_following(self, user):
         return self.followed.filter(
             followers.c.followed_id == user.id).count() > 0
+
+    def followed_posts(self):
+        followed = Post.query.join(
+            followers, (followers.c.followed_id == Post.user_id)).filter(
+                followers.c.follower_id == self.id)
+        own = Post.query.filter_by(user_id=self.id)
+        return followed.union(own).order_by(Post.timestamp.desc())
+
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.String(140))
