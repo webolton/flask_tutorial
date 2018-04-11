a3431c0bc3d967464e8511689d071c38bb27013f
William Bolton <william.ellet@gmail.com>
2018-04-10 20:31:02 -0400

    Improve posts functionality

--- app/routes.py
+++ app/routes.py
@@ -23,16 +23,7 @@ def index():
         db.session.commit()
         flash('Your post is now live!')
         return redirect(url_for('index'))
-    posts = [
-        {
-            'author': { 'username': 'John' },
-            'body': 'Beautiful day in Melbourne!'
-        },
-        {
-            'author': { 'username': 'Susan' },
-            'body': 'I have had a nice breakfast'
-        }
-    ]
+    posts = current_user.followed_posts().all()
     return render_template('index.html', title='Home', form=form, posts=posts)
 
 @app.route('/login', methods=['GET', 'POST'])
