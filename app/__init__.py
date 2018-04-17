7d6368a77860678b07f95d08b1a58ce8e1f9d563
William Bolton <william.ellet@gmail.com>
2018-04-17 04:05:21 -0400

    Add Bootstrap

--- app/__init__.py
+++ app/__init__.py
@@ -7,6 +7,7 @@ from flask_login import LoginManager
 import logging
 from logging.handlers import SMTPHandler, RotatingFileHandler
 import os
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -15,24 +16,25 @@ migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
 mail = Mail(app)
+bootstrap = Bootstrap(app)
 
 from app import routes, models, errors
 
 if not app.debug:
-    if app.config['MAIL_SERVER']:
-        auth = None
-    if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
-        auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
-    secure = None
-    if app.config['MAIL_USE_TLS']:
-        secure = ()
-    mail_handler = SMTHandler(
-        mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
-        fromaddr='no-reply@' + app.config['MAIL_SERVER'],
-        toaddrs=app.config['ADMINS'], subject='Microblog Failure',
-        credentials=auth, secure=secure)
-    mail_handler.setLevel(logging.ERROR)
-    app.logger.addHandler(mail_handler)
+    # if app.config['MAIL_SERVER']:
+    #     auth = None
+    # if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
+    #     auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
+    # secure = None
+    # if app.config['MAIL_USE_TLS']:
+    #     secure = ()
+    # mail_handler = SMTPHandler(
+    #     mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
+    #     fromaddr='no-reply@' + app.config['MAIL_SERVER'],
+    #     toaddrs=app.config['ADMINS'], subject='Microblog Failure',
+    #     credentials=auth, secure=secure)
+    # mail_handler.setLevel(logging.ERROR)
+    # app.logger.addHandler(mail_handler)
 
     if not os.path.exists('logs'):
         os.mkdir('logs')
